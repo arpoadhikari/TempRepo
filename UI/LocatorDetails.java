f0a39e789d572e9dd8575521a0e316df07197a9c
arpoadhikari <arpoadhikari@gmail.com>
2018-06-08 19:38:30 +0530

    Create LocatorDetails.java

---
+++ UI/LocatorDetails.java
@@ -0,0 +1,16 @@
+package UI;
+
+import java.util.List;
+
+public class LocatorDetails {
+
+	private List<String> locator;
+
+	public List<String> getLocator() {
+		return locator;
+	}
+
+	public void setLocator(List<String> locator) {
+		this.locator = locator;
+	}
+}
