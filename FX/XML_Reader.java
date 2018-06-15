4f3dd50538b6a9a2be45b003be71e94b62f44c1e
arpoadhikari <arpoadhikari@gmail.com>
2018-06-15 19:25:59 +0530

    Update XML_Reader.java

--- FX/XML_Reader.java
+++ FX/XML_Reader.java
@@ -1,6 +1,7 @@
 package application.support;
 
 import javax.xml.parsers.DocumentBuilderFactory;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
@@ -10,10 +11,17 @@ import javax.xml.xpath.XPathFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 
+/**
+ * XML Reader class
+ *
+ */
 public class XML_Reader {
 
 	private Document doc;
 
+	/** Constructor of the XML_Reader CLass
+	 * @param xmlPath
+	 */
 	public XML_Reader(String xmlPath) {
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder builder;
@@ -21,14 +29,14 @@ public class XML_Reader {
 			builder = factory.newDocumentBuilder();
 			this.doc = builder.parse(xmlPath);
 		} catch (Exception e) {
-			System.out.println("ERROR! --- Unable to read XML.");
+			System.out.println("ERROR! --- Unable to read XML : "+xmlPath+"\n"+e);
 		}
 	}
 
-	public static void main(String[] args) {
-
-	}
-
+	/** Returns matching node list based on the xpath
+	 * @param xpathExpr
+	 * @return NodeList
+	 */
 	public NodeList getNodeListFromXPATH(String xpathExpr) {
 		try {
 			XPathFactory xPathfactory = XPathFactory.newInstance();
@@ -37,7 +45,7 @@ public class XML_Reader {
 			NodeList nodeList = (NodeList) expr.evaluate(this.doc, XPathConstants.NODESET);
 			return nodeList;
 		} catch (Exception e) {
-			System.out.println("ERROR! --- Unbale to evaluate the xpath expression.");
+			System.out.println("ERROR! --- Unbale to evaluate the xpath expression : "+xpathExpr+"\n"+e);
 			return null;
 		}
 	}
