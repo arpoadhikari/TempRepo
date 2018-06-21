c1d4bf1ae70e8b9ec3205a2b5baddf3bec7d98c8
arpoadhikari <arpoadhikari@gmail.com>
2018-06-21 19:06:21 +0530

    Update Main.java

--- FX/Main.java
+++ FX/Main.java
@@ -9,17 +9,20 @@ import application.views.MainViewController;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
 
 	public static Stage primaryStage;
+	public static Stage mergerStage;
 	public static String appTitle = "GTAF Object Repository Manager";
 
 	@Override
@@ -27,22 +30,53 @@ public class Main extends Application {
 		primaryStage = stage;
 		primaryStage.setTitle(appTitle);
 
+		mergerStage = new Stage();
+		mergerStage.setTitle("Object Repository Merger");
+
 		try {
-			FXMLLoader loader = new FXMLLoader(Main.class.getResource("views/MainView.fxml"));
-			VBox page = loader.load();
-			Scene scene = new Scene(page);
-			//setUserAgentStylesheet(STYLESHEET_CASPIAN);
-			primaryStage.setScene(scene);
+
+			//Initializes the main window
+			FXMLLoader primaryLoader = new FXMLLoader(Main.class.getResource("views/MainView.fxml"));
+			VBox primaryPage = primaryLoader.load();
+			Scene primaryScene = new Scene(primaryPage);
+			primaryStage.setScene(primaryScene);
 			primaryStage.setMinHeight(650);
 			primaryStage.setMinWidth(1000);
 			primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("icons/appIcon.png")));
+
+			//Initializes the merger window
+			FXMLLoader mergerLoader = new FXMLLoader(Main.class.getResource("views/MergerView.fxml"));
+			AnchorPane mergerPage = mergerLoader.load();
+			Scene mergerScene = new Scene(mergerPage);
+			mergerStage.setScene(mergerScene);
+			mergerStage.setMinHeight(650);
+			mergerStage.setMinWidth(1000);
+			mergerStage.getIcons().add(new Image(Main.class.getResourceAsStream("icons/merge.png")));
+			mergerStage.initModality(Modality.WINDOW_MODAL);
+			mergerStage.initOwner(Main.primaryStage);
+
 			primaryStage.show();
 
 			primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
 				public void handle(WindowEvent we) {
-					Optional<ButtonType> option = DialogViewController.showConfirmation("Confirmation", "Confirm Exit", "Are you sure you want to exit ?");
+					Optional<ButtonType> option = DialogViewController.showConfirmation("Close Application", "Confirm Exit", "Are you sure you want to exit ?");
 					if (option.get() != ButtonType.OK) {
 						we.consume();
+					}
+					else {
+						primaryStage.close();
+					}
+				}
+			});
+
+			mergerStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+				public void handle(WindowEvent we) {
+					Optional<ButtonType> option = DialogViewController.showConfirmation("Close Application", "Confirm Exit", "Are you sure you want to exit ?");
+					if (option.get() != ButtonType.OK) {
+						we.consume();
+					}
+					else {
+						mergerStage.close();
 					}
 				}
 			});
